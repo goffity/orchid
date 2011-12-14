7db0e96003effa1586879ec1c9c34af5a30a2743
goffity <goffity@gmail.com>
2011-12-15 01:00:08 +0700

    2011-12-15 	- disease 	- flow search

--- result_search.php
+++ result_search.php
@@ -9,6 +9,9 @@ $result=mysql_query($query,$Connect)or die(mysql_error());
 //$result2=mysql_query($query,$Connect)or die(mysql_error());
 
 $row=mysql_fetch_array($result);
+
+$_SESSION['key_word'] = $chk_s;
+$_SESSION['key_id'] = $row['key_id'];
 //echo $row['key_id'];
 
 ?>
@@ -43,12 +46,74 @@ $row=mysql_fetch_array($result);
 					href="<?php bloginfo('url');?>/?s=<?php echo $set0['key_name'];?>"><?php echo "[".$z."]  ".$set0['key_name'] ;?>
 				</a></li></td>
 		</tr>
-		<?php $z++;}?>
-		
-		<?php 
-		//query search_type
+		<?php
+
+		$_SESSION['src_type_id'] = $set0['search_type_id'];
+
+		$z++;
+		}
 		?>
-		
+
+		<?php
+		//query search_type
+		//echo "ID: ".$_SESSION['src_type_id'];
+		$sql = "SELECT `search_type`.`search_type_id`, `search_type`.`search_name` FROM `search_type` WHERE (`search_type_id` = '".$_SESSION['src_type_id']."');";
+		//echo $sql."<br>";
+		$query_result_search_type = mysql_query($sql,$Connect) or die(mysql_errno());
+		$search_type_result = mysql_fetch_array($query_result_search_type);
+		//echo "XXXX: ".empty($search_type_result);
+
+		$_SESSION['src_name'] = $search_type_result['search_name'];
+
+		//search family id
+		if($_SESSION['src_name'] == "disease"){
+			$sql = "SELECT
+						`disease_id`
+    					, `disease_name_th`
+    					, `disease_name_eng`
+    					, `disease_characteristic`
+    					, `disease_url`
+    					, `disease_source`
+    					, `disease_status`
+    					, `disease_update_date`
+					FROM
+    					`disease`
+    				WHERE 
+    					(`disease_name_th` ='".$_SESSION['key_word']."')
+    					OR 
+    					(`disease_name_eng` ='".$_SESSION['key_word']."');";
+
+			$query_disease_id = mysql_query($sql,$Connect) or die(mysql_errno());
+			$search_disease_id = mysql_fetch_array($query_disease_id);
+
+			//query disease ref
+			$sql = "SELECT
+    					`disease_id`
+    					,`family_id`
+					FROM
+    					`disease_ref`
+					WHERE 
+						(`disease_id` ='".$search_disease_id['disease_id']."');";
+
+			$query_fam_id = mysql_query($sql,$Connect) or die(mysql_errno());
+			$search_fam_id = mysql_fetch_array($query_fam_id);
+
+			$_SESSION['fam_id'] = $search_fam_id['family_id'];
+
+			//TODO add dis to array
+
+		}else {
+			$sql = "SELECT `family_id` FROM ".$_SESSION['src_name']." WHERE `key_id` = '".$_SESSION['key_id']."'";
+			//echo "SQL: ".$sql;
+			$query_result_fam_id = mysql_query($sql,$Connect) or die(mysql_errno());
+			$search_fam_id = mysql_fetch_array($query_result_fam_id);
+
+			$_SESSION['fam_id'] = $search_fam_id['family_id'];
+		}
+
+
+		//query
+		?>
 
 		<tr>
 			<td height="30" bgcolor="#E3E9F1"><span style="padding-left: 20px;"><b>ชื่อสกุล</b>
@@ -58,11 +123,13 @@ $row=mysql_fetch_array($result);
 
 		//query หาชื่อ สกุล จาก table keywordRef , orchid varity , family
 		$sql_1="SELECT family.family_id,family.FamilyName_TH AS FamName, family.FamilyID AS FamID
-		FROM family
-		LEFT JOIN orchid_varity
-		ON(family.FamilyID=orchid_varity.FamilyID)
-		WHERE
-		orchid_varity.key_id='".$row['key_id']."'";
+		 FROM family
+		 LEFT JOIN orchid_varity
+		 ON(family.FamilyID=orchid_varity.FamilyID)
+		 WHERE
+		 orchid_varity.key_id='".$row['key_id']."'";
+
+		$sql_1 = "SELECT `family_id` , `family_name_th` , `family_name_eng` , `family_characteristic` FROM `family` WHERE (`family_id` = '".$_SESSION['fam_id']."');";
 
 		$sql_1_result=mysql_query($sql_1,$Connect) or die(mysql_error());
 		$set1_result=mysql_fetch_array($sql_1_result);
@@ -72,7 +139,7 @@ $row=mysql_fetch_array($result);
 		?>
 		<tr>
 			<td style="padding-left: 30px;"><a
-				href="<?php bloginfo('url');?>/?s=<?php echo $set1_result['FamName'] ;?>"><?php echo $set1_result['FamName'] ;?>
+				href="<?php bloginfo('url');?>/?s=<?php echo $set1_result['family_name_th'] ;?>"><?php echo $set1_result['family_name_th']." (". $set1_result['family_name_eng'].")" ;?>
 			</a></td>
 		</tr>
 
@@ -85,15 +152,17 @@ $row=mysql_fetch_array($result);
 
 		//echo "XXXX ".$_SESSION['fam_id'];
 		$query_ord = "SELECT
-		 				`orchid_varity`.`orchid_name_th`
-		 				,`family`.`family_id`
-		 			FROM
-		 				`orchid_varity`
-		 				INNER JOIN `wordpress2`.`family`
-		 				ON (`orchid_varity`.`family_id` = `family`.`family_id`)
-		 			WHERE
-		 				`orchid_varity`.`family_id` ='".$_SESSION['fam_id']."' 
-		 				AND `family`.`family_id` ='".$_SESSION['fam_id']."';";
+    					`orchid_id`
+    					, `orchid_name_th`
+    					, `ochid_name_eng`
+    					, `orchid_content1`
+    					, `orchid_url`
+    					, `orchid_status`
+    					, `orchid_update_date`
+    					, `family_id`
+					FROM
+    					`orchid_varity`
+					WHERE (`family_id` ='".$_SESSION['fam_id']."');";
 
 		$sql_2_result=mysql_query($query_ord,$Connect) or die(mysql_error());
 
@@ -113,13 +182,31 @@ $row=mysql_fetch_array($result);
 		</tr>
 		<?php
 		//query หาโรคที่เกี่ยวข้อง จ่าก table disease , diseaseRef และ Family
-		$query_ds=" SELECT DieseaseName_TH
+		/*$query_ds=" SELECT DieseaseName_TH
 			FROM disease
 			LEFT JOIN disease_relation
 			ON(disease.DiseaseID=disease_relation.DiseaseID)
 			WHERE
 			disease_relation.FamilyID='".$set1_result['FamID']."'
-";
+			";*/
+
+		$query_ds="SELECT
+    				`disease_id`
+    				, `disease_name_th`
+    				, `disease_name_eng`
+    				, `disease_characteristic`
+    				, `disease_url`
+    				, `disease_source`
+    				, `disease_status`
+    				, `disease_update_date`
+				FROM
+    				`disease`
+    			WHERE 
+    				(`disease_id` IN (
+    					SELECT `disease_ref`.`disease_id` FROM `disease_ref` WHERE `disease_ref`.`family_id` = '".$_SESSION['fam_id']."'
+    					)
+    				);";
+
 		$sql_3_result=mysql_query($query_ds,$Connect)or die(mysql_error());
 
 
@@ -127,7 +214,7 @@ $row=mysql_fetch_array($result);
 		while($set3_result=mysql_fetch_array($sql_3_result)){?>
 		<tr>
 			<td style="padding-left: 30px;"><li><a
-					href="<?php bloginfo('url');?>/?s=<?php echo $set3_result['DieseaseName_TH'];?>"><?php echo "[".$b."]  ".$set3_result['DieseaseName_TH'] ;?>
+					href="<?php bloginfo('url');?>/?s=<?php echo $set3_result['disease_name_th'];?>"><?php echo "[".$b."]  ".$set3_result['disease_name_th']." (".$set3_result['disease_name_eng'].")" ;?>
 				</a></li></td>
 		</tr>
 		<?php $b++; } ?>
